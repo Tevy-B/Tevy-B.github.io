f0517523db4e4e4f8b95f5febba42eb156b9bfaa
sethateybong <sethatevy98@gmail.com>
2022-02-20 13:19:59 -0500

    staged chnage

---
+++ .vscode/alive/fasl/tmp.lisp
@@ -0,0 +1,16 @@
+(BLOCK NIL
+  (LET ((X NIL)
+        (#:LOOP-LIST-559
+         (SB-KERNEL:THE* (LIST :USE-ANNOTATIONS T :SOURCE-FORM '(A B C D E))
+                         '(A B C D E))))
+    (DECLARE (IGNORABLE #:LOOP-LIST-559)
+             (IGNORABLE X))
+    (TAGBODY
+     SB-LOOP::NEXT-LOOP
+      (SETQ X (CAR #:LOOP-LIST-559))
+      (SETQ #:LOOP-LIST-559 (CDR #:LOOP-LIST-559))
+      (PRINT X)
+      (IF (ENDP #:LOOP-LIST-559)
+          (GO SB-LOOP::END-LOOP))
+      (GO SB-LOOP::NEXT-LOOP)
+     SB-LOOP::END-LOOP)))
